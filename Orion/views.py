369253e3a4d47a37eb0b47e45cf8fee170054e4c
Jhin Yao <yaojing@ufotosoft.com>
2021-05-08 19:07:17 +0800

    login function

---
+++ Orion/views.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# @author: Lazy Yao
+# @email: none
+# @date: 2021/05/08 16:33
+
+from django.shortcuts import HttpResponse, render, redirect
+
+
+def login(request):
+    return render(request, "login.html")
